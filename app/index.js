04eb923173772ed068cabde79c5ebfa34f6c5d7d
damien.gallet <damien.gallet@insa-lyon.fr>
2016-12-12 20:26:09 +1100

    First dialog, but with a problem in the interactive response

--- app/index.js
+++ app/index.js
@@ -1,35 +1,35 @@
 'use strict';
 
+// Wit includes
 let Wit = require('node-wit').Wit;
 let interactive = require('node-wit').interactive;
 
+// General purpose includes
+var moment = require('moment');
+
+// Wit access token for the TravelHelper chat
 const accessToken = '7LXX326XSRNCZ2ESA44QYOC6IMXRRC2J';
-
-/*const readlineModule = require('readline');
-
-const readline = readlineModule.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-var question = 'Should we start ?';
-readline.question(question,
-                  (answer) => {
-                    console.log('Let\'s go !');
-                  });*/
 
 var actions = {
   send (request, response) {
     const {sessionId, context, entities} = request;
     const {text, quickreplies} = response;
     return new Promise((resolve, reject) => {
-      console.log(response.text);
+      if(text) {
+        console.log(response.text);
+        console.log(response)
+      } else {
+        console.log('The response contains a quick reply, we cannot manage it for now');
+      }
       return resolve();
     });
   },
+
+  // calculateDuration : given a start and an end date,
+  //                     give the duration in days
   calculateDuration ({context, entities}) {
     return new Promise((resolve, reject) => {
-      context.duration = 15;
+      console.log(JSON.stringify(entities));
       return resolve(context);
     });
   }
